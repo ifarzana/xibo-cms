a4d3070a126c6bbddc0f9387f6fc03eab8d68827
ifarzana <m2farzana@gmail.com>
2023-11-07 21:32:56 +0000

    bb

--- cypress/e2e/displays.cy.js
+++ cypress/e2e/displays.cy.js
@@ -53,7 +53,7 @@ describe('Displays', function() {
 
     // Wait for the grid reload
     cy.wait('@loadGridAfterSearch');
-    cy.get('#displays tbody tr').should('have.length', 1);
+    cy.get('#displays tbody tr').should('have.length', 3);
 
     // Click on the first row element to open the delete modal
     cy.get('#displays tr:first-child .dropdown-toggle').click();
@@ -99,7 +99,7 @@ describe('Displays', function() {
 
     // Wait for the grid reload
     cy.wait('@loadGridAfterSearch');
-    cy.get('#displays tbody tr').should('have.length', 1);
+    cy.get('#displays tbody tr').should('have.length', 3);
 
     // Click on the first row element to open the delete modal
     cy.get('#displays tr:first-child .dropdown-toggle').click();
