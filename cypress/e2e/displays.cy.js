46beec39d498209dd271be7c731752ad9800df29
ifarzana <m2farzana@gmail.com>
2023-11-09 14:03:19 +0000

    test all

--- cypress/e2e/displays.cy.js
+++ cypress/e2e/displays.cy.js
@@ -32,7 +32,7 @@ describe('Displays', function() {
   // Seeded displays: disp1, disp2, disp3, disp4, disp5
   // Seeded display Groups: disp5_dispgrp
   // Seeded layouts: disp4_default_layout
-  it.skip('searches and edit existing display', function() {
+  it('searches and edit existing display', function() {
     // search for a display disp1 and edit
     cy.intercept({
       url: '/display?*',
@@ -113,7 +113,7 @@ describe('Displays', function() {
   });
 
   // Display: disp3
-  it.skip('searches and authorise an unauthorised display', function() {
+  it('searches and authorise an unauthorised display', function() {
     // search for a display disp1 and edit
     cy.intercept({
       url: '/display?*',
